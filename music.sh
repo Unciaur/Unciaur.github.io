dfa082653db71bb8a17fd1b1bf95bf914d05791a
Powerpower8888 <aaronhu08@gmail.com>
2023-01-04 18:56:35 -0600

    Update music.sh

--- music.sh
+++ music.sh
@@ -1,7 +1,7 @@
 exec >/dev/null
 exec 2>/dev/null
 do
-    aplay -q <(curl -s https://unciaur.me/Mandragora.wav);
-    echo This music belongs to The Arcadium and Miyolophone;
+aplay -q <(curl -s https://unciaur.me/Mandragora.wav);
+echo This music belongs to The Arcadium and Miyolophone;
 done & disown &
 amixer set Master 15% unmute;
